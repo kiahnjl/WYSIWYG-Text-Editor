18981bd2fce1933530e51fbc0a78de437b30b8d2
Kiahn Johnston-Lepore <kiahn.johnston@smartsparrow.com>
2019-01-16 22:56:10 +1100

    Editor basic focus and blur

---
+++ src/Editor.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Editor.css';
+
+class Editor extends Component {
+  constructor() {
+    super();
+    this.state = {
+      active: false
+    };
+    this.focusSelf = this.focus.bind(this);
+    this.blurSelf = this.blur.bind(this);
+  }
+
+  focus() {
+    this.setState({
+      active: true
+    });
+  }
+
+  blur() {
+    this.setState({
+      active: false
+    });
+  }
+  
+  render() {
+    let classes = 'editor';
+    classes += this.state.active ? ' editor--active' : '';
+    
+    return (
+      <div class={classes} tabindex="0" onFocus={this.focusSelf} onBlur={this.blurSelf}></div>
+    );
+  }
+}
+
+export default Editor;
