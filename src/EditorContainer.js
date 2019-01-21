6e4f436c237c9b0bd5b4f0fb7dfea83b7e37ffa4
kiahnjl <kiahnjl@gmail.com>
2019-01-21 23:29:01 +1100

    Not sure how to structure data either.

--- src/EditorContainer.js
+++ src/EditorContainer.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import Editor from './Editor';
 
+/*
+ * Data.
+ * How should the data of a text editor be stored?
+ * What does the data structure look like?
+ * What are the concerns for the structure?
+ * - need to be able to delete/insert at any point
+ * - need to be able to delete/insert ranges of text
+ *
+ * pros/cons:
+ * plain text:
+ * - slow delete/insert in middle?
+ * - simple
+ * - easy view, easy transform
+ *
+ * tree:
+ * - how is it a tree structure?
+ *
+ * array:
+ * - fast access to any row
+ *
+ * other:
+ * ?
+ * */
+
 class EditorContainer extends Component {
   constructor() {
     super();
     this.state = {
-      data: [''],
-      text: ''
+      data: ['']
     };
     this.selfOnType = this.onType.bind(this);
   }
 
   onType(key) {
     this.setState((state) => ({
-      data: getNextData(state.data, key),
-      text: state.text + key
+      data: getNextData(state.data, key)
     }));
   }
   
@@ -26,19 +48,27 @@ class EditorContainer extends Component {
 }
 
 function getNextData(data, key) {
+  // index should be the cursor position
+  let index = Math.max(0, data.length - 1);
+
   if(key === 'Enter') {
     data.push('');
     return data;
-  } else if(key === 'Backspace' || key === 'Shift') {
+  } else if(key === 'Shift' || key === 'Meta') {
+    return data;
+  } else if(key === 'Backspace') {
+    if(data[index].length > 0) {
+      data[index] = data[index].substring(0, data[index].length - 1);
+    } else if(data.length > 1) {
+      data.splice(index, 1);
+    }
+    return data;
+  } else {
+    data[index] = data[index] || '';
+    data[index] += key;
+
     return data;
   }
-  
-  let index = data.length === 0 ? 0 : data.length - 1;
-
-  data[index] = data[index] || '';
-  data[index] += key;
-  
-  return data;
 }
 
 export default EditorContainer;
