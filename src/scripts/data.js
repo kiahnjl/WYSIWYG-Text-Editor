17332ca7f479fdb65fc4252437d55566f543708d
kiahnjl <kiahnjl@gmail.com>
2019-02-16 12:08:01 +1100

    Cleanup and esc html in text content

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -7,17 +7,39 @@
   var Type = {
     TITLE: 0,
     BOLD: 1,
-    ITALIC: 2
+    ITALIC: 2,
+    TEXT: 3,
+    NEWLINE: 4
   };
   
   var Tag = {
     0: 'h1',
     1: 'strong',
-    2: 'i'
+    2: 'i',
+    3: 'span',
+    4: 'p'
+  };
+
+  var safeText = function(text) {
+      return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
   };
   
   var buildNode = function(type, text) {
     return '<' + Tag[type] + '>' + text + '</' + Tag[type] + '>';
+  };
+
+  var builder = function(content, id) {
+    var curr = content[id];
+    
+    if(!curr.children) {
+      return buildNode(curr.type, safeText(curr.text));
+    } else if(curr.children) {
+      return curr.children.map(function(childId) {
+        return buildNode(curr.type, safeText(curr.text) + builder(content, childId));
+      });
+    } else if(curr.next) {
+      return buildNode(curr.type, safeText(curr.text) + builder(content, curr.next));
+    }
   };
   
   var create = function() {
@@ -34,17 +56,7 @@
     };
     
     var getHtml = function() {
-      var builder = function(curr) {
-        if(!curr.children) {
-          return buildNode(curr.type, curr.text);
-        } else {
-          return curr.children.map(function(childId) {
-            return buildNode(curr.type, builder(Content[childId]));
-          });
-        }
-      };
-      
-      return builder(Content[0]);
+      return builder(Content, 0);
     };
     
     return {
